16ae4ab9802d954fab336e6451018f7f6964ddf7
Viery Nugroho <viery15102002@gmail.com>
2024-09-26 18:54:22 +0700

    add: init, puzzle game logic & user-friendly UI

---
+++ index.js
@@ -0,0 +1,86 @@
+let rows = 3;
+let columns = 3;
+let type;
+
+let currTile;
+let otherTile; //blank tile
+
+let turns = 0;
+
+let imageOrder = ['4', '2', '8', '5', '1', '6', '7', '9', '3'];
+
+function playGame(gameType) {
+	localStorage.setItem('gameType', gameType);
+	window.location.href = `/${gameType}.html`;
+}
+
+window.onload = function () {
+	for (let r = 0; r < rows; r++) {
+		for (let c = 0; c < columns; c++) {
+			let tile = document.createElement('img');
+			tile.id = r.toString() + '-' + c.toString();
+			tile.src = `./assets/pattern/${localStorage.getItem('gameType')}/` + imageOrder.shift() + '.jpg';
+
+			//DRAG FUNCTIONALITY
+			tile.addEventListener('dragstart', dragStart); //click an image to drag
+			tile.addEventListener('dragover', dragOver); //moving image around while clicked
+			tile.addEventListener('dragenter', dragEnter); //dragging image onto another one
+			tile.addEventListener('dragleave', dragLeave); //dragged image leaving anohter image
+			tile.addEventListener('drop', dragDrop); //drag an image over another image, drop the image
+			tile.addEventListener('dragend', dragEnd); //after drag drop, swap the two tiles
+
+			document.getElementById('board').append(tile);
+		}
+	}
+};
+
+function dragStart() {
+	currTile = this;
+}
+
+function dragOver(e) {
+	e.preventDefault();
+}
+
+function dragEnter(e) {
+	e.preventDefault();
+}
+
+function dragLeave() {}
+
+function dragDrop() {
+	otherTile = this; //this refers to the img tile being dropped on
+}
+
+function dragEnd() {
+	if (!otherTile.src.includes('3.jpg')) {
+		return;
+	}
+
+	let currCoords = currTile.id.split('-'); //ex) "0-0" -> ["0", "0"]
+	let r = parseInt(currCoords[0]);
+	let c = parseInt(currCoords[1]);
+
+	let otherCoords = otherTile.id.split('-');
+	let r2 = parseInt(otherCoords[0]);
+	let c2 = parseInt(otherCoords[1]);
+
+	let moveLeft = r == r2 && c2 == c - 1;
+	let moveRight = r == r2 && c2 == c + 1;
+
+	let moveUp = c == c2 && r2 == r - 1;
+	let moveDown = c == c2 && r2 == r + 1;
+
+	let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+	if (isAdjacent) {
+		let currImg = currTile.src;
+		let otherImg = otherTile.src;
+
+		currTile.src = otherImg;
+		otherTile.src = currImg;
+
+		turns += 1;
+		document.getElementById('turns').innerText = turns;
+	}
+}
